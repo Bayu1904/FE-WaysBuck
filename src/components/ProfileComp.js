6267bda502d1b1575320cb29d19c22eed6f21952
Kev2Cod <kevinity310@gmail.com>
2022-08-28 22:43:41 +0700

    productimg

--- src/components/ProfileComp.js
+++ src/components/ProfileComp.js
@@ -33,13 +33,13 @@ export default function ProfileComp() {
                 <p className="mb-0" style={{ color: "#613D2B", fontSize: 25 }}>
                   Full Name
                 </p>
-                <p>{transaction[0].user?.name}</p>
+                <p>{transaction?.user?.name}</p>
               </div>
               <div className="fw-semibold">
                 <p className="mb-0" style={{ color: "#613D2B", fontSize: 25 }}>
                   Email
                 </p>
-                <p>{transaction[0].user?.email}</p>
+                <p>{transaction?.user?.email}</p>
               </div>
             </Col>
           </Row>
